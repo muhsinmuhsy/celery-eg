df4e08c5f0bbd89291d117ca46a269121f754ee3
Muhsinws <muhsin@weamse.in>
2024-07-24 15:41:22 +0530

    first commit

--- django_celery_project/celery.py
+++ django_celery_project/celery.py
@@ -1,20 +1,22 @@
-from __future__ import absolute_import, unicode_literals
 import os
 
 from celery import Celery
-from django.conf import settings
 from celery.schedules import crontab
 
+# Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_celery_project.settings')
 
 app = Celery('django_celery_project')
-app.conf.enable_utc = False
 
-app.conf.update(timezone = 'Asia/Kolkata')
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object('django.conf:settings', namespace='CELERY')
 
-app.config_from_object(settings, namespace='CELERY')
+# Load task modules from all registered Django apps.
+app.autodiscover_tasks()
 
-# Celery Beat Settings
 app.conf.beat_schedule = {
     'send-mail-every-day-at-8': {
         'task': 'send_mail_app.tasks.send_mail_func',
@@ -24,10 +26,6 @@ app.conf.beat_schedule = {
     
 }
 
-# Celery Schedules - https://docs.celeryproject.org/en/stable/reference/celery.schedules.html
-
-app.autodiscover_tasks()
-
-@app.task(bind=True)
+@app.task(bind=True, ignore_result=True)
 def debug_task(self):
     print(f'Request: {self.request!r}')
